dd8a03238b09cb95aae78093d09ea3af7a639d94
voltex-at <voltex.at@gmail.com>
2026-09-23 23:30:32 +0200

    Add hardware-safe melody audio engine

---
+++ include/AudioManager.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <Arduino.h>
+#include "CalendarEngine.h"
+
+enum class MelodyType : uint8_t {
+  CHRISTMAS,
+  BIRTHDAY,
+  HALLOWEEN,
+  NEW_YEAR,
+  EASTER,
+  VACATION
+};
+
+class AudioManager {
+public:
+  void begin();
+  void stop();
+  void play(MelodyType melody);
+  void playForScene(SceneType scene);
+  bool isPlaying() const { return playing_; }
+
+private:
+  bool playing_ = false;
+  int8_t sineLut_[256]{};
+
+  void enableDac();
+  void playTone(uint16_t frequency, uint16_t durationMs);
+  void playRest(uint16_t durationMs);
+};
